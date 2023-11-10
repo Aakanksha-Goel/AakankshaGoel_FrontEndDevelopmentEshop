41ef54e5609e17d408a680f34fa577beb2348e0d
Sanket Anand <sanketsfdc34@gmail.com>
2023-11-11 01:26:21 +0530

    Created Initial Components for the Project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import ShoppingCart from '@mmaterial-ui/icons/ShoppingCart';
+import AppBar from '@material-ui/core/AppBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
