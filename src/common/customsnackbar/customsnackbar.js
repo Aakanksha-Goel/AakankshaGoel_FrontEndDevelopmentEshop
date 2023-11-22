c82aad12cc743039cc7482bcb07f36825e865e4f
sumitkumar23 <sumit@codebase.com>
2023-11-22 08:41:18 +0530

    /users endpoint for user sign up added and .gitignore added

--- src/common/customsnackbar/customsnackbar.js
+++ src/common/customsnackbar/customsnackbar.js
@@ -1,18 +1,17 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
 
 const Alert = React.forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
-
-export default function PositionedSnackbar({message, typeOfSnackBar}) {
+export default function PositionedSnackbar({ message, typeOfSnackBar }) {
   const [state, setState] = React.useState({
     open: true,
-    vertical: 'top',
-    horizontal: 'right',
+    vertical: "top",
+    horizontal: "right",
   });
 
   const { vertical, horizontal, open } = state;
@@ -23,17 +22,21 @@ export default function PositionedSnackbar({message, typeOfSnackBar}) {
 
   return (
     <Box sx={{ width: 500 }}>
-      <Snackbar 
+      <Snackbar
         anchorOrigin={{ vertical, horizontal }}
-        open={state.open}
+        open={open}
         autoHideDuration={6000}
         onClose={handleClose}
         key={vertical + horizontal}
       >
-  <Alert onClose={handleClose} severity={typeOfSnackBar} sx={{ width: '100%' }}>
-    {message}
-  </Alert>
-</Snackbar>
+        <Alert
+          onClose={handleClose}
+          severity={typeOfSnackBar}
+          sx={{ width: "100%" }}
+        >
+          {message}
+        </Alert>
+      </Snackbar>
     </Box>
   );
 }
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -4,93 +4,93 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import Container from "@mui/material/Container";
-import { useSelector, useDispatch, shallowEqual } from 'react-redux';
 import CssBaseline from "@mui/material/CssBaseline";
 import Avatar from "@mui/material/Avatar";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
-import SearchAppBar from '../../common/navbar/navbar';
-import {useState} from 'react';
+import SearchAppBar from "../../common/navbar/navbar";
+import { Copyright } from "../../common/Copyright";
+import { useState } from "react";
 import PositionedSnackbar from "../../common/customsnackbar/customsnackbar";
+import { useNavigate } from "react-router-dom";
 
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://upgrad.com/">
-        upGrad
-      </Link>{" "}
-      2021
-      {"."}
-    </Typography>
-  );
-}
-
 const defaultTheme = createTheme();
 
-const SelectUsers = (state) => state.users;
-
 export default function SignUp() {
+  const navigate = useNavigate();
+  const [value, setValue] = useState("");
+  const [showSnackBar, setShowSnackBar] = useState({
+    show: false,
+    message: "",
+    type: "",
+  });
 
-  const users = useSelector(SelectUsers, shallowEqual);
-
-  const [value, setValue] = useState('');
-
-  let showSnackBar = false;
-
-  const handleChange = event => {
-    const result = event.target.value.replace(/\D/g, '');
-
+  const handleChange = (event) => {
+    const result = event.target.value.replace(/\D/g, "");
     setValue(result);
   };
 
-  const dispatch = useDispatch();
-
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
 
     let newUser = {
-      id: users.length,
       firstName: data.get("fname"),
       lastName: data.get("lname"),
       email: data.get("email"),
-      role: 'sales',
+      role: "sales",
       password: data.get("password"),
-      contactNumber: data.get("contact_number")
-
+      contactNumber: data.get("contact_number"),
     };
 
-    users.users.forEach((item) => {
-        if(item.email === newUser.email && item.password === newUser.password){
-          showSnackBar = true;
-          return;
-        }else{
-          showSnackBar = false;
-        }
+    // Make a POST request to the API
+    const response = await fetch("http://0.0.0.0:8080/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
     });
-
-    users.activeUser = newUser;
-    users.users.push(newUser);
-
-    sessionStorage.setItem("userCache", JSON.stringify(users));
-
-    window.location.href = '/home';
-    
+    const result = response.json();
+    result
+      .then((res) => {
+        if (res?.success) {
+          setShowSnackBar({
+            show: true,
+            message: res?.message || "Successfully Signed Up!",
+            type: "success",
+          });
+          setTimeout(() => navigate("/"), 2000);
+        } else {
+          setShowSnackBar({
+            show: true,
+            message: res?.detail || "Failed to Sign Up!",
+            type: "error",
+          });
+        }
+      })
+      .catch((err) => {
+        setShowSnackBar({
+          show: true,
+          message: "Failed Signed Up. Try again!",
+          type: "error",
+        });
+      });
   };
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <SearchAppBar/>
-      <PositionedSnackbar dismissOrNot={showSnackBar} message={'User Already Exists !!!'} typeOfSnackBar={'error'}/>
+      <SearchAppBar />
+      {showSnackBar.show && (
+        <PositionedSnackbar
+          dismissOrNot={true}
+          message={showSnackBar.message}
+          typeOfSnackBar={showSnackBar.type}
+        />
+      )}
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
@@ -98,7 +98,7 @@ export default function SignUp() {
             marginTop: 8,
             display: "flex",
             flexDirection: "column",
-            alignItems: "center"
+            alignItems: "center",
           }}
         >
           <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
@@ -107,12 +107,7 @@ export default function SignUp() {
           <Typography component="h1" variant="h5">
             Sign Up
           </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            
-            sx={{ mt: 1 }}
-          >
+          <Box component="form" onSubmit={handleSubmit} sx={{ mt: 1 }}>
             <TextField
               margin="normal"
               required
@@ -146,7 +141,7 @@ export default function SignUp() {
               margin="normal"
               required
               fullWidth
-              inputProps={{ maxLength: 12, minlength: 8 }}
+              inputProps={{ maxLength: 12, minLength: 8 }}
               name="password"
               label="Password"
               type="password"
@@ -157,7 +152,7 @@ export default function SignUp() {
               margin="normal"
               required
               fullWidth
-              inputProps={{ maxLength: 12, minlength: 8 }}
+              inputProps={{ maxLength: 12, minLength: 8 }}
               name="confirm_password"
               label="Confirm Password"
               type="password"
@@ -183,7 +178,7 @@ export default function SignUp() {
             >
               Sign Up
             </Button>
-            <Grid  sx={{  display: "block", textAlign: "end" }} container>
+            <Grid sx={{ display: "block", textAlign: "end" }} container>
               <Grid item>
                 <Link href="/" variant="body2">
                   {"Already have an account? Sign In"}
